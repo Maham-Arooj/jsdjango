3311c62da96a9adbcf518d6a6d85424838fa3cbf
Maham-Arooj <maham123arooj@gmail.com>
2021-07-01 10:34:34 +0500

    update

--- accounts/__init__.py
+++ accounts/__init__.py
@@ -9,8 +9,3 @@
 #urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 #print(static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
 
-#urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-#print(static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
-
-#urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-#print(static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
---
+++ accounts/migrations/__init__.py
@@ -0,0 +1,7 @@
+#urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#print(static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
+
+
+#urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#print(static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
+
