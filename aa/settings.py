65bfb8fc301820f98348c2193b3403f45ae3f357
Maham-Arooj <86724195+Maham-Arooj@users.noreply.github.com>
2021-07-04 02:28:39 +0500

    Update settings.py

--- aa/settings.py
+++ aa/settings.py
@@ -137,7 +137,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
     ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
