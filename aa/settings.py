d101580f4a97f8a90684c7e35a977424e840ac7f
Maham-Arooj <86724195+Maham-Arooj@users.noreply.github.com>
2021-07-10 20:30:28 +0500

    Update settings.py

--- aa/settings.py
+++ aa/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6t)c55+52s!!jq5_#6e4g!0m=y(eftrm&velz5v#0#4o-i8bi8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['jsdjango.herokuapp.com','127.0.0.1']
 
